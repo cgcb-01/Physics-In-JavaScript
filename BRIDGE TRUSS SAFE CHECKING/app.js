6c920d6291c8c66c11fbb622bf19991aa1f73ea0
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-06-19 23:47:28 +0530

    Plan overview rough idea to work on

---
+++ BRIDGE TRUSS SAFE CHECKING/app.js
@@ -0,0 +1,10 @@
+/*  PLAN OVERVIEW
+
+1. First click line button initiate length and and angle of rotation of angle according create a base line.
+2. Now when the line forms drag and drop feature.
+3. Screw option clicking give screw image.
+4. Fixing the screw on a bar shows length from surrounding bars automatically.
+5. Assistance bar on side to guide.
+6. 
+
+*/
